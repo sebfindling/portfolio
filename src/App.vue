f3116872dcae590c6d9c61b46925783216964dea
Seb Findling <i@seb.cl>
2024-10-08 22:18:10 -0300

    dark mode with DarkReader

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,17 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
+import { RouterView } from 'vue-router'
+import { onMounted } from 'vue';
+
+// Dark mode (DarkReader)
+onMounted(async () => {
+  if (typeof window === 'undefined') return;
+  const { auto } = await import('darkreader');
+  auto({
+    brightness: 100,
+    contrast: 90,
+    sepia: 10,
+  });
+});
 </script>
 
 <template>
