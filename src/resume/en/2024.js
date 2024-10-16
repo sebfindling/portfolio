6da2f3d9acfad897677221160cd0669b3e861bfb
Seb <i@seb.cl>
2024-10-16 07:33:03 -0300

    better wording

--- src/resume/en/2024.js
+++ src/resume/en/2024.js
@@ -2,7 +2,7 @@ export default {
     items: [{
         title: 'SAMO',
         description: 'Modular management software for issuing electronic bills and taxes, with a\
-        highly scalable infrastructura for an easy future growth.',
+        highly scalable infrastructura that supports seamless future growth.',
         images: [
             { src: 'samo1.jpg' },
             { src: 'samo2.jpg' },
