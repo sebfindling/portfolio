6b013dd44d644f82c77c0b4dc2fbfb6102f1351a
Sebastian Findling <i@seb.cl>
2025-10-13 04:33:36 -0300

    fix

--- src/resume/en/1996.js
+++ src/resume/en/1996.js
@@ -1,13 +1,13 @@
 export default {
-    items: [
-        {
-            title: 'My first program',
-            description: 'At the age of 9, thanks to a cousin who taught me BASIC programming language, I created my first piece of code.',
-            images: [
-                {src: 'seba.gif', alt: ''},
-                {src: 'seba.bas.jpg', alt: 'My program in Notepad on Windows 3.11'},
-                {src: 'seba.jpg', alt: 'First steps in Microsoft QBASIC.'},
-            ]
-        },
-    ],
+  items: [
+    {
+      title: 'My first program',
+      description: 'At the age of 8, I was introduced to the BASIC programming language by a cousin, which enabled me to create my first piece of code.',
+      images: [
+        { src: 'seba.gif', alt: '' },
+        { src: 'seba.bas.jpg', alt: 'My program in Notepad on Windows 3.11' },
+        { src: 'seba.jpg', alt: 'First steps in Microsoft QBASIC.' },
+      ]
+    },
+  ],
 }
